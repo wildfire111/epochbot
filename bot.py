1139b1e82bb96e1b9f4b28a7326e6f1ad20239fb
wildfire111 <106965615+wildfire111@users.noreply.github.com>
2022-09-29 00:12:32 +1000

    fixed bugs, made prettier. added uwu guild id

--- bot.py
+++ bot.py
@@ -6,9 +6,10 @@ import time
 import asyncio
 from dotenv import load_dotenv
 load_dotenv()
+print('loaded env')
 token = os.getenv('DISCORD_TOKEN')
-guild = os.getenv('GUILD_ID')
-
+#guild = 978102149420560414 #THIS IS ALREADY THE UWU ID
+guild = 993978823471276172
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
@@ -29,14 +30,16 @@ async def update():
     minutes = minutes % 60
     days = hours // 24
     hours = hours % 24
-    nickname = f'UwUpoch end: {target.month}/{target.day}/{target.year} {target.hour}:{target.minute}'
-    status = f'Remaining: {days}D {hours}H {minutes}M'
+    nickname = f'UwUpoch: {target.month}/{target.day}'# {target.hour}:{target.minute}'
+    status = f'for: {days}D {hours}H {minutes}M'
     await bot.get_guild(guild).me.edit(nick=nickname)
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status))
     time.sleep(60)
     await update()
 
+@bot.event
 async def on_ready():
+    print('ready!')
     await update()
 
 bot.run(token)
